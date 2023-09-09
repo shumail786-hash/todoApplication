5936bcc8adc9da9f6961e85b3c2126732e2fe86c
shumail786-hash <sherrymines@outlook.com>
2023-09-09 20:09:29 +0500

    UI Changed

--- src/components/UploadTodo/uploadTodo.jsx
+++ src/components/UploadTodo/uploadTodo.jsx
@@ -5,7 +5,8 @@ import { toast } from "react-toastify";
 const UploadTodo = () => {
   const [inputValue, setinputValue] = useState("");
   const [sortTodo, setSortTodo] = useState([]);
-
+  const [currentDay, setCurrentDay] = useState("");
+  const [currentTime, setCurrentTime] = useState(new Date());
   //   Checking whether data is stored in Local Storage or not. If yes
   // then parse the data otherwise return the empty array.
 
@@ -17,12 +18,24 @@ const UploadTodo = () => {
 
   // checking length of incomplete tasks
   const [length, setLength] = useState(Number);
+  const [completeLength, setCompletedLength] = useState(Number);
+
+  // Editing the li
+
+  const [editType, setEditType] = useState(false);
+  const [editedText, setEditedText] = useState("");
+
   const submitValueToLocalStorage = (e) => {
     e.preventDefault();
     if (inputValue === "") {
       toast.error("Empty Value is not Accepted");
     } else {
-      const postValue = { id: Date.now(), value: inputValue, statusOfTodo };
+      const postValue = {
+        id: Date.now(),
+        value: inputValue,
+        statusOfTodo,
+        editType,
+      };
       setSubmitTodo((previousTodo) => [...previousTodo, postValue]);
       setinputValue("");
       //   Store now in Local Storage
@@ -40,6 +53,17 @@ const UploadTodo = () => {
       setLength(checkLength);
     }
   };
+
+  const checkCompleteLengthHandler = () => {
+    const checkLength = submitTodo.filter(
+      (element) => element.statusOfTodo === true
+    );
+    if (checkLength.length === 0) {
+      setCompletedLength(0);
+    } else {
+      setCompletedLength(checkLength);
+    }
+  };
   // console.log(length.length);
 
   useEffect(() => {
@@ -50,8 +74,37 @@ const UploadTodo = () => {
       setSortTodo(arrayOfData);
     }
 
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDate = new Date();
+    const date = currentDate.getDate();
+    const dayOfWeek = daysOfWeek[currentDate.getDay()];
+    const month = currentDate.toLocaleString("default", { month: "long" });
+    // Getting Local Time
+
+    // Formatting the time data as "Month, Day Date"
+    const formattedDate = `${dayOfWeek} ${month}, ${date}`;
+    setCurrentDay(formattedDate);
+    // Formatting hours and minutes
+
     checkLengthHandler();
-  }, [submitTodo, statusOfTodo]);
+    checkCompleteLengthHandler();
+
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [submitTodo, statusOfTodo, editType]);
   // console.log(sortTodo, "I'm after");
   //   console.log(submitTodo);
 
@@ -60,8 +113,33 @@ const UploadTodo = () => {
   const handleRemoveList = (id) => {
     const filterTodo = submitTodo.filter((item) => item.id !== id);
     setSubmitTodo(filterTodo);
+    toast.info("Task Deleted Successfully");
     localStorage.setItem("todo", JSON.stringify(filterTodo));
     // console.log(filterTodo);
+  };
+
+  const editTogglerMode = (id) => {
+    const updatedTodo = submitTodo.map((item) =>
+      item.id === id ? { ...item, editType: !item.editType } : item
+    );
+    setSubmitTodo(updatedTodo);
+    localStorage.setItem("todo", JSON.stringify(updatedTodo));
+  };
+  const handleEditList = (id) => {
+    const filterTodo = submitTodo.map((element) =>
+      element.id === id
+        ? {
+            ...element,
+            value: editedText,
+            editType: !element.editType,
+            statusOfTodo: false,
+          }
+        : element
+    );
+    setEditedText("");
+    setSubmitTodo(filterTodo);
+    toast.success("Saved Successfully");
+    localStorage.setItem("todo", JSON.stringify(filterTodo));
   };
   const handleStatusofTodo = (id) => {
     const updatedTodo = submitTodo.map((item) =>
@@ -72,16 +150,47 @@ const UploadTodo = () => {
   };
 
   const clearCompletedTasks = () => {
-    const filterTodo = submitTodo.filter((element) => !element.statusOfTodo);
-    // console.log(filterTodo);
-    setSubmitTodo(filterTodo);
-    localStorage.setItem("todo", JSON.stringify(filterTodo));
+    const hasCompleted = submitTodo.some(
+      (element) => element.statusOfTodo === true
+    );
+    if (hasCompleted) {
+      const filterTodo = submitTodo.filter((element) => !element.statusOfTodo);
+      setSubmitTodo(filterTodo);
+      toast.success("Completed Tasks have cleared");
+      localStorage.setItem("todo", JSON.stringify(filterTodo));
+    } else {
+      toast.error("No Completed Task Found");
+    }
   };
   return (
     <>
       <form action="submit" onSubmit={submitValueToLocalStorage}>
         <div className="app__content">
-          <h1>Your Todo List:</h1>
+          <div className="app__content_system_time">
+            <div className="app__content_day_date">
+              <p>{currentDay}</p>
+              {length.length === undefined ? (
+                <li>0 Active Task.</li>
+              ) : (
+                <li>{length.length} Active Task.</li>
+              )}
+            </div>
+            <div className="app__content_time">
+              <p>
+                {currentTime.toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  second: "2-digit",
+                  hour12: true,
+                })}
+              </p>
+              {completeLength.length === undefined ? (
+                <li>0 Completed Tasks </li>
+              ) : (
+                <li> {completeLength.length} Completed Tasks </li>
+              )}
+            </div>
+          </div>
           <div className="app__input">
             <input
               type="text"
@@ -100,31 +209,57 @@ const UploadTodo = () => {
               <>
                 {sortTodo.map((element, index) => (
                   <div className="app__lists_li_btn" key={index}>
-                    <li
-                      className={`${
-                        element.statusOfTodo === false
-                          ? "app__lists_li"
-                          : "app__lists_li disabledLi"
-                      }`}
-                      onClick={() => handleStatusofTodo(element.id)}
-                    >
-                      {index + 1}. {element.value}
-                      {"  "}
-                      {element.statusOfTodo === true ? (
-                        <span className="app__status_completed">
-                          {" "}
-                          (Completed)
-                        </span>
-                      ) : (
-                        <></>
-                      )}
-                    </li>
-                    <button
-                      className="app__lists_li_del"
-                      onClick={() => handleRemoveList(element.id)}
-                    >
-                      Delete
-                    </button>
+                    {element.editType ? (
+                      <>
+                        <input
+                          type="text"
+                          placeholder="Edit"
+                          value={editedText}
+                          onChange={(e) => setEditedText(e.target.value)}
+                          className="app__lists_li inputLi"
+                        />
+                        <button
+                          className="app__lists_li_edit app__lists_li_save"
+                          onClick={() => handleEditList(element.id)}
+                        >
+                          Save
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        <li
+                          className={`${
+                            element.statusOfTodo === false
+                              ? "app__lists_li"
+                              : "app__lists_li disabledLi"
+                          }`}
+                          onClick={() => handleStatusofTodo(element.id)}
+                        >
+                          {index + 1}. {element.value}
+                          {"  "}
+                          {element.statusOfTodo === true ? (
+                            <span className="app__status_completed">
+                              {" "}
+                              (Completed)
+                            </span>
+                          ) : (
+                            <></>
+                          )}
+                        </li>
+                        <button
+                          className="app__lists_li_edit"
+                          onClick={() => editTogglerMode(element.id)}
+                        >
+                          Update
+                        </button>
+                        <button
+                          className="app__lists_li_del"
+                          onClick={() => handleRemoveList(element.id)}
+                        >
+                          Delete
+                        </button>
+                      </>
+                    )}
                   </div>
                 ))}
               </>
@@ -135,18 +270,9 @@ const UploadTodo = () => {
             )}
           </ul>
         </div>
-        <div className="app__options">
-          <div className="app__options_index">
-            {length.length === undefined ? (
-              <li>0 Task Left.</li>
-            ) : (
-              <li>{length.length} Task(s) Left.</li>
-            )}
-          </div>
 
-          <div className="app__options_tasks_status">
-            <li onClick={clearCompletedTasks}>Clear Complete</li>
-          </div>
+        <div className="app__options_tasks_status">
+          <li onClick={clearCompletedTasks}>Clear Complete</li>
         </div>
       </div>
     </>
